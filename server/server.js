0db7bb08eef7d9ad3f42043f2d5b02a3bf08c70c
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-05 22:37:13 +0530

    Fixed Server

--- server/server.js
+++ server/server.js
@@ -1,29 +1,32 @@
-import express from "express"
-import { configDotenv } from "dotenv"
-import DbConnection from "./config/connectToDb.js"
-import userRouter from "./routes/userRoutes.js"
-import taskRouter from "./routes/taskRoutes.js"
-import projectRouter from "./routes/projectRoutes.js"
-import companyRouter from "./routes/companyRoutes.js"
-import updateRouter from "./routes/updateRoutes.js"
-import commentRouter from "./routes/commentRoutes.js"
-import stageRouter from "./routes/stageRoutes.js"
+import express from "express";
+import { configDotenv } from "dotenv";
+import DbConnection from "./config/connectToDb.js";
+import userRouter from "./routes/userRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
+import companyRouter from "./routes/companyRoutes.js";
+import updateRouter from "./routes/updateRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import stageRouter from "./routes/stageRoutes.js";
 
-configDotenv()
+configDotenv();
 
-const server = express()
+const server = express();
 
-DbConnection()
+DbConnection();
 
-server.use("api/v1/users", userRouter)
-server.use("api/v1/tasks", taskRouter)
-server.use("api/v1/company", companyRouter)
-server.use("api/v1/project", projectRouter)
-server.use("api/v1/stage", stageRouter)
-server.use("api/v1/update", updateRouter)
-server.use("api/v1/comment", commentRouter)
+server.use("api/v1/users", userRouter);
+server.use("api/v1/tasks", taskRouter);
+server.use("api/v1/company", companyRouter);
+server.use("api/v1/project", projectRouter);
+server.use("api/v1/stage", stageRouter);
+server.use("api/v1/update", updateRouter);
+server.use("api/v1/comment", commentRouter);
 
+server.use("*", () => {
+  console.log("Not Found");
+});
 
-server.listen(process.env.PORT, ()=> {
-    console.log("Server Is Running On PORT", process.env.PORT)
-})
+server.listen(process.env.PORT, () => {
+  console.log("Server Is Running On PORT", process.env.PORT);
+});
