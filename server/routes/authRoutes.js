228b68a7164574f977ad29ad65679c7b1af8e687
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-07 19:36:44 +0530

    Added Some Comments

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,12 @@ import { login, signup } from "../controllers/authController";
 
 const authRouter = express.Router()
 
+// @Route : api/v1/auth/login
+// @Description : API For login
 authRouter.route("/login").post(login)
+
+// @Route : api/v1/auth/signup
+// @Description : API For Signup
 authRouter.route("/signup").post(signup)
 
 export default authRouter
