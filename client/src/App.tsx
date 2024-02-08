97d485ecba34b131cb850739f55ed80d25b3909b
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-02-08 18:57:40 +0530

    Core integrations +++

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,36 @@ import MainNavigator from "./navigation/MainNavigator";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import UserDataContext from "./context/UserDataContext";
 import { useState } from "react";
+import GlobalSnackbar from "./ui/Snackbar";
+import { SnackbarTypes } from "./types/SnackbarTypes";
+import SnackbarContext from "./context/SnackbarContext";
 
 function App() {
   const [userData, setUserData] = useState({});
+  const [openSnack, setOpenSnack] = useState<SnackbarTypes>({
+    open: false,
+    message: "no message",
+    severity: "success",
+  });
 
   return (
     <UserDataContext.Provider value={{ userData, setUserData }}>
-      <ThemeProvider theme={ProjectTheme}>
-        <Box
-          sx={{
-            minWidth: "100%",
-            minHeight: "100%",
-            color: "text.primary",
-            background: "background.default",
-          }}
-        >
-          <MainNavigator />
-          <ReactQueryDevtools />
-        </Box>
-      </ThemeProvider>
+      <SnackbarContext.Provider value={{ openSnack, setOpenSnack }}>
+        <ThemeProvider theme={ProjectTheme}>
+          <Box
+            sx={{
+              minWidth: "100%",
+              minHeight: "100%",
+              color: "text.primary",
+              background: "background.default",
+            }}
+          >
+            <GlobalSnackbar value={{ openSnack, setOpenSnack }} />
+            <MainNavigator />
+            <ReactQueryDevtools />
+          </Box>
+        </ThemeProvider>
+      </SnackbarContext.Provider>
     </UserDataContext.Provider>
   );
 }
